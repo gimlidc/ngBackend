cccbfd2b35d7fee838781dcf5e255a1822b74272
Honza Blazek <gimli@matfyz.cz>
2017-06-01 11:38:19 +0200

    added user details

--- routes/users.js
+++ routes/users.js
@@ -38,6 +38,36 @@ router.get('/', function (req, res, next) {
     res.send('[{"name": "Pepa", "surname": "Novák", "birthYear": 1920},{"name": "Franta", "surname": "Liška", "birthYear": 2010}]');
 });
 
+router.get('/:id/details', function (req, res) {
+    switch(parseInt(req.params.id)) {
+        case 1:
+            res.send({"name": "Pepa", "surname": "Novák", "birthYear": 1920});
+            break;
+        case 2:
+            res.send({
+                "name": "Franta",
+                "surname": "Liška",
+                "birthYear": 2010,
+                "address": {
+                    "street": "Jehovova",
+                    "streetNumber": "1250b",
+                    "city": "Praha",
+                    "zipCode": "11000",
+                    "country": "Czech Republic"
+                },
+                "email": "franta.liska@gmail.com",
+                "phoneNumber": "+420777189999",
+                "personalIdentificationNumber": "101010/0300"
+            });
+            break;
+        case 3:
+            res.send({"name": "Pan", "surname": "Někdo", "birthYear": 1980});
+            break;
+        default:
+            res.status(404).send({"error": "User not found"});
+    }
+});
+
 /* PUT new users listing. */
 router.put('/new', validate({body: userSchema}), function (req, res) {
     res.send("user accepted");
